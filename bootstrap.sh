83106775b43e7f5bc17adf2f340c8492aa08beb5
Cory Imdieke <cory.imdieke@cityelectricsupply.com>
2020-08-08 23:11:58 -0500

    New settings for zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,9 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function doIt() {
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
-	source ~/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
